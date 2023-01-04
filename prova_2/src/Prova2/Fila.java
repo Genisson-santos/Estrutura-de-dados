135d08f1b6691facfc85597a94c4e06825df3deb
Genisson-santos <105828791+Genisson-santos@users.noreply.github.com>
2023-01-03 21:10:26 -0300

    Update Fila.java

--- prova_2/src/Prova2/Fila.java
+++ prova_2/src/Prova2/Fila.java
@@ -20,23 +20,23 @@ public void remover() {
 this.fila.remover_lista(fila.getinicial().getElemento());
 	}
 
-public boolean saoIguais(Fila<T> fila1, Fila<T> fila2) {
-	// filas auxiliares pra guardar informaÁ„o
+public boolean saoIguais(IFila<T> fila1, IFila<T> fila2) {
+	// filas auxiliares pra guardar informa√ß√£o
 	 Fila filaux1 =new Fila();
      Fila filaux2 =new Fila();
      int tamanhoaux =fila1.getTamanho();
      //se o tamanho das filas for diferente retorna falso
 	 if (fila1.getTamanho()!= fila2.getTamanho()||fila2.getTamanho()!= fila1.getTamanho()) {
-		 System.out.println("As filas N„o S„o Iguais / TAMANHOS DIFERENTES");
+		 System.out.println("As filas N√£o S√£o Iguais / TAMANHOS DIFERENTES");
 		 System.out.println("Fila 1: "+fila1.toString());
 			System.out.println("Fila 2: "+fila2.toString());
          return false;   
 	}
 	 //emquanto as filas estiverem cheias vai incluir os elementos da fila original na auxiliar
 	 
-	 //comparar se o primeiro elemento das duas filas s„o iguais, se for desempilha
+	 //comparar se o primeiro elemento das duas filas s√£o iguais, se for desempilha
 	 
-	 //se as filas originais estiverem vaizas significa que as filas saı iguais retorna verdadeiro
+	 //se as filas originais estiverem vaizas significa que as filas sa√µ iguais retorna verdadeiro
 	 
 	 //e se os primeiros das filas forem diferentes retorna falso;
 	 
@@ -48,7 +48,7 @@ public boolean saoIguais(Fila<T> fila1, Fila<T> fila2) {
 			 fila1.remover();
 			 fila2.remover();
 			 if(fila1.estaVazia()  && fila2.estaVazia() ){
-				 System.out.println("As filas S„o Iguais");
+				 System.out.println("As filas S√£o Iguais");
 				 System.out.println("Tamanho das filas: "+tamanhoaux);
 				System.out.println("Fila 1: "+filaux1.toString());
 				System.out.println("Fila 2: "+filaux2.toString());
@@ -57,7 +57,7 @@ public boolean saoIguais(Fila<T> fila1, Fila<T> fila2) {
 			 }
 		 if(fila1.getPrimeiro()!=fila2.getPrimeiro()||fila2.getPrimeiro()!=fila1.getPrimeiro()) {
 			 if(tamanhoaux!=fila1.getTamanho()||tamanhoaux==fila1.getTamanho()||tamanhoaux!=fila2.getTamanho()||tamanhoaux==fila2.getTamanho() ) { 
-			 System.out.println("As filas N„o S„o Iguais / CONTEUDO DIFERENTE");
+			 System.out.println("As filas N√£o S√£o Iguais / CONTEUDO DIFERENTE");
 			 System.out.println("Tamanho das filas: "+tamanhoaux);
 			 System.out.println("Fila 1: "+filaux1.toString()+fila1.toString());
 				System.out.println("Fila 2: "+filaux2.toString()+fila2.toString());
