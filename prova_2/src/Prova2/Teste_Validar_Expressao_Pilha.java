58c2eb6af280ba0961bae7a68e5a1ca2e917a6e0
Genisson-santos <105828791+Genisson-santos@users.noreply.github.com>
2023-01-03 21:53:12 -0300

    mudança de nome expressão pra expressao

--- prova_2/src/Prova2/Teste_Validar_Expressao_Pilha.java
+++ prova_2/src/Prova2/Teste_Validar_Expressao_Pilha.java
@@ -3,22 +3,22 @@ package Prova2;
 public class Teste_Validar_Expressao_Pilha {
 	public static void main(String[] args) {
 
-//Express„o v·lida
+//Express√£o v√°lida
 
 		String expressao = ("A + [ B *(C + D) ] * E");
 
-//Express„o inv·lida   
+//Express√£o inv√°lida   
 
 		 //String expressao = ("A + B * (C + D] * E");
 
-		Pilha_analizar_express„o expressao_entrada = new Pilha_analizar_express„o(expressao);
+		Pilha_analizar_expressao expressao_entrada = new Pilha_analizar_expressao(expressao);
 		System.out.println(expressao_entrada.toString());
 		//System.out.println(expressao_entrada.validarExpressao());
 		
 		if (expressao_entrada.validarExpressao()) {
-			System.out.println("Express„o valida!");
+			System.out.println("Express√£o valida!");
 		} else {
-			System.out.println("Express„o invalida!");
+			System.out.println("Express√£o invalida!");
 		}
 	}
 
